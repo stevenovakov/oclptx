4305ee156b847c1e8852cf61d0085b43cdb2a0cd
stevenovakov <stevenovakov@gmail.com>
2014-03-31 17:31:12 -0700

    Add timer code in main function

--- main.cc
+++ main.cc
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <cassert>
 #include <thread>
+#include <chrono>
 
 #include "fifo.h"
 #include "oclenv.h"
@@ -87,6 +88,9 @@ int main(int argc, char **argv)
 
   env.CreateKernels("standard");
 
+  auto t_end = std::chrono::high_resolution_clock::now();
+  auto t_start = std::chrono::high_resolution_clock::now();
+
   global_fd = fopen("./path_output", "w");
   if (NULL == global_fd)
   {
@@ -111,6 +115,9 @@ int main(int argc, char **argv)
   OclPtxHandler *handler = new OclPtxHandler[num_dev];
   std::thread *gpu_managers[num_dev];
 
+  // may want to change the location of this later
+  t_start = std::chrono::high_resolution_clock::now();
+
   for (int i = 0; i < num_dev; ++i)
   {
     handler[i].Init(env.GetContext(),
@@ -132,6 +139,11 @@ int main(int argc, char **argv)
   {
     gpu_managers[i]->join();
   }
+
+  t_end = std::chrono::high_resolution_clock::now();
+    std::cout<< "Total Tracking Time (ns): " <<
+        std::chrono::duration_cast<std::chrono::nanoseconds>(
+          t_end-t_start).count() << std::endl;
   delete[] handler;
 
   fclose(global_fd);
